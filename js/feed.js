30492de666c12f3e3d495e2aa25058a755275d7e
tasnee101 <te1904679@qu.edu.qa>
2026-03-21 16:50:27 +0300

    feed JS

--- js/feed.js
+++ js/feed.js
@@ -1,97 +1,3 @@
-// const CreatedPostsList = [];
-
-
-// // function to display all posts have been created by our user
-// function DisplayingPosts(){
-//     const feed = document.querySelector("#feed");
-
-//     if(CreatedPostsList.length===0){
-//         feed.innerHTML=`<p>No posts yet</p>`
-//         return;
-//     }
-//     feed.innerHTML = "";
-//     CreatedPostsList.forEach(function(item){
-//         feed.innerHTML += `<p>${item.post}</p>`;
-//     });
-// }
-
-
-
-// // for the submit listerner , for data to be pushed in the feed
-
-// const form = document.querySelector("#creat-post-form");
-// form.addEventListener("submit",function(e){
-//     e.preventDefault();
-//     const createdPost = document.querySelector("#post-text").value;
-
-//     CreatedPostsList.push({
-//         post: createdPost
-//     });
-
-//     DisplayingPosts();
-//     form.reset();
-
-// });
-
-// const clear = document.querySelector("#clear-btn");
-// clear.addEventListener("click",function(){
-//     CreatedPostsList.length = 0;
-//     DisplayingPosts();
-// })
-
-
-
-// let counter = 0;
-// const likeBtn = document.getElementById("like-btn");
-// const likeCount = document.getElementById("like-count");
-
-// likeBtn.addEventListener("click", function() {
-//     counter++;
-//     likeCount.textContent = counter;
-//     // console.log(`Like count: ${counter}`); 
-//     const starIcon = document.querySelector("#star-icon");
-//     if (starIcon.src.includes("no-color-star.png")) {
-//         starIcon.src = "media/icons/star.png";
-//     } else {
-//         starIcon.src = "media/icons/no-color-star.png";
-//     }        
-//     });
-
-
-
-
-
-// form submit listner
-
-// const form = document.querySelector("#personal-info-form");
-// form.addEventListener("submit",function(e){
-//     e.preventDefault();
-//     const firstname = document.querySelector("#first-name").value;
-//     const lastname = document.querySelector("#last-name").value;
-//     const email = document.querySelector("#email").value;
-//     const pass = document.querySelector("#pass").value;
-
-//     accountList.push({
-//        FName : firstname,
-//        LName : lastname,
-//        Email : email,
-//        Pass : pass
-//     });
-
-//     localStorage.setItem("users",JSON.stringify(accountList));
-//     form.reset();
-
-//     window.location.href = "login_signup.html";
-// });
-
-// document.querySelector("clear-btn").addEventListener("click",function(){
-//     form.reset();
-// })
-
-
-// here , we will check if the user already have an account, 
-// otherwise , we will add this new user to localStorage
-
 class PostManager{
     constructor(){
         this.storgekey = "MyPosts";
@@ -99,10 +5,12 @@ class PostManager{
         this.posts = saved? JSON.parse(saved):[];
     }
     addPost(username,post){
-        const postContent = this.post;
-        const connect = this.posts.find(t=>t.username === this.username)
-                                    .add(t=>t.post +=this.post);
-        this.posts.push(connect);
+        const postContent = {
+            username: username,
+            post: post,
+            createdAt: new Date().toLocaleString()
+        }
+        this.posts.unshift(postContent);
         this.save();
     }
     getAll(){
@@ -113,37 +21,46 @@ class PostManager{
         localStorage.setItem(this.storgekey,JSON.stringify(this.posts));
     }
     
-    login(username,password){
-        const found = this.accounts.find(t=>t.username === username && t.password === password);
-        if(!found){
-            console.error("Invalid username or password");
-            return;
-        }
-        return found;
+    }
+    
+
+
+
+const manager  = new PostManager();
+
+function DisplayingPosts(){
+    const feed  = document.querySelector("#feed");
+    if(manager.getAll().length === 0){
+        feed.innerHTML = `<p>No posts yet</p>`
+        return;
+    }
+    else{
+        feed.innerHTML = ""
+        manager.getAll().forEach(t => {
+            feed.innerHTML+=`${t.post}`; 
+        });
     }
 }
+DisplayingPosts();
 
-
-// -------------------------
-
-const manager  = new AccountManager();
 
 const submitListener = document.querySelector("#submit-btn");
 submitListener.addEventListener("click",function(e){
     e.preventDefault();
-    const firstname = document.querySelector("#first-name").value;
-    const lastname = document.querySelector("#last-name").value;
-    const username = document.querySelector("#user-name").value;
-    const email = document.querySelector("#email").value;
-    const password = document.querySelector("#pass").value;
-    manager.add(firstname,lastname,username,email,password);
-    document.querySelector("#personal-info-form").reset();
-    window.location.href = "login.html"
-});
-
-
+    const createdPost = document.querySelector("#post").value;
+    const username = localStorage.getItem("loggedInUser");
+    if(createdPost === "")return;
+    manager.addPost(username,createdPost);
+    DisplayingPosts();
+    document.querySelector("#create-post-form").reset();
+    
+})
 const clearListener = document.querySelector("#clear-btn");
 clearListener.addEventListener("click",function(e){
-    document.querySelector("#personal-info-form").reset();
-});
+    document.querySelector("#create-post-form").reset();
+})
+
+
+
+
 
