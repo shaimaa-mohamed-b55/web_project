b48ceaf64203d5286cf088c9a7db1b19de544c59
tasnee101 <te1904679@qu.edu.qa>
2026-03-21 04:49:15 +0300

    JS for sign up

--- js/feed.js
+++ js/feed.js
@@ -1,59 +1,149 @@
-const CreatedPostsList = [];
+// const CreatedPostsList = [];
 
 
-// function to display all posts have been created by our user
-function DisplayingPosts(){
-    const feed = document.querySelector("#feed");
+// // function to display all posts have been created by our user
+// function DisplayingPosts(){
+//     const feed = document.querySelector("#feed");
 
-    if(CreatedPostsList.length===0){
-        feed.innerHTML=`<p>No posts yet</p>`
-        return;
+//     if(CreatedPostsList.length===0){
+//         feed.innerHTML=`<p>No posts yet</p>`
+//         return;
+//     }
+//     feed.innerHTML = "";
+//     CreatedPostsList.forEach(function(item){
+//         feed.innerHTML += `<p>${item.post}</p>`;
+//     });
+// }
+
+
+
+// // for the submit listerner , for data to be pushed in the feed
+
+// const form = document.querySelector("#creat-post-form");
+// form.addEventListener("submit",function(e){
+//     e.preventDefault();
+//     const createdPost = document.querySelector("#post-text").value;
+
+//     CreatedPostsList.push({
+//         post: createdPost
+//     });
+
+//     DisplayingPosts();
+//     form.reset();
+
+// });
+
+// const clear = document.querySelector("#clear-btn");
+// clear.addEventListener("click",function(){
+//     CreatedPostsList.length = 0;
+//     DisplayingPosts();
+// })
+
+
+
+// let counter = 0;
+// const likeBtn = document.getElementById("like-btn");
+// const likeCount = document.getElementById("like-count");
+
+// likeBtn.addEventListener("click", function() {
+//     counter++;
+//     likeCount.textContent = counter;
+//     // console.log(`Like count: ${counter}`); 
+//     const starIcon = document.querySelector("#star-icon");
+//     if (starIcon.src.includes("no-color-star.png")) {
+//         starIcon.src = "media/icons/star.png";
+//     } else {
+//         starIcon.src = "media/icons/no-color-star.png";
+//     }        
+//     });
+
+
+
+
+
+// form submit listner
+
+// const form = document.querySelector("#personal-info-form");
+// form.addEventListener("submit",function(e){
+//     e.preventDefault();
+//     const firstname = document.querySelector("#first-name").value;
+//     const lastname = document.querySelector("#last-name").value;
+//     const email = document.querySelector("#email").value;
+//     const pass = document.querySelector("#pass").value;
+
+//     accountList.push({
+//        FName : firstname,
+//        LName : lastname,
+//        Email : email,
+//        Pass : pass
+//     });
+
+//     localStorage.setItem("users",JSON.stringify(accountList));
+//     form.reset();
+
+//     window.location.href = "login_signup.html";
+// });
+
+// document.querySelector("clear-btn").addEventListener("click",function(){
+//     form.reset();
+// })
+
+
+// here , we will check if the user already have an account, 
+// otherwise , we will add this new user to localStorage
+
+class PostManager{
+    constructor(){
+        this.storgekey = "MyPosts";
+        const saved  = localStorage.getItem(this.storgekey);
+        this.posts = saved? JSON.parse(saved):[];
     }
-    feed.innerHTML = "";
-    CreatedPostsList.forEach(function(item){
-        feed.innerHTML += `<p>${item.post}</p>`;
-    });
+    addPost(username,post){
+        const postContent = this.post;
+        const connect = this.posts.find(t=>t.username === this.username)
+                                    .add(t=>t.post +=this.post);
+        this.posts.push(connect);
+        this.save();
+    }
+    getAll(){
+        return this.posts;
+    }
+
+    save(){
+        localStorage.setItem(this.storgekey,JSON.stringify(this.posts));
+    }
+    
+    login(username,password){
+        const found = this.accounts.find(t=>t.username === username && t.password === password);
+        if(!found){
+            console.error("Invalid username or password");
+            return;
+        }
+        return found;
+    }
 }
 
 
+// -------------------------
 
-// for the submit listerner , for data to be pushed in the feed
+const manager  = new AccountManager();
 
-const form = document.querySelector("#creat-post-form");
-form.addEventListener("submit",function(e){
+const submitListener = document.querySelector("#submit-btn");
+submitListener.addEventListener("click",function(e){
     e.preventDefault();
-    const createdPost = document.querySelector("#post-text").value;
-
-    CreatedPostsList.push({
-        post: createdPost
-    });
-
-    DisplayingPosts();
-    form.reset();
-
+    const firstname = document.querySelector("#first-name").value;
+    const lastname = document.querySelector("#last-name").value;
+    const username = document.querySelector("#user-name").value;
+    const email = document.querySelector("#email").value;
+    const password = document.querySelector("#pass").value;
+    manager.add(firstname,lastname,username,email,password);
+    document.querySelector("#personal-info-form").reset();
+    window.location.href = "login.html"
 });
 
-const clear = document.querySelector("#clear-btn");
-clear.addEventListener("click",function(){
-    CreatedPostsList.length = 0;
-    DisplayingPosts();
-})
 
-
-
-let counter = 0;
-const likeBtn = document.getElementById("like-btn");
-const likeCount = document.getElementById("like-count");
-
-likeBtn.addEventListener("click", function() {
-    counter++;
-    likeCount.textContent = counter;
-    // console.log(`Like count: ${counter}`); 
-    const starIcon = document.querySelector("#star-icon");
-    if (starIcon.src.includes("no-color-star.png")) {
-        starIcon.src = "../../media/icons/star.png";
-    } else {
-        starIcon.src = "../../media/icons/no-color-star.png";
-    }        
-    });
+const clearListener = document.querySelector("#clear-btn");
+clearListener.addEventListener("click",function(e){
+    document.querySelector("#personal-info-form").reset();
+});
 
--- js/signup.js
+++ js/signup.js
@@ -1,37 +1,103 @@
-// here we have to save data from registeration
-
-// idk from where to start honestly 
-
-// lets start by creating an array to store users data
-
-const accountList = [];
-
 // form submit listner
 
-const form = document.querySelector("#personal-info-form");
-form.addEventListener("submit",function(e){
+// const form = document.querySelector("#personal-info-form");
+// form.addEventListener("submit",function(e){
+//     e.preventDefault();
+//     const firstname = document.querySelector("#first-name").value;
+//     const lastname = document.querySelector("#last-name").value;
+//     const email = document.querySelector("#email").value;
+//     const pass = document.querySelector("#pass").value;
+
+//     accountList.push({
+//        FName : firstname,
+//        LName : lastname,
+//        Email : email,
+//        Pass : pass
+//     });
+
+//     localStorage.setItem("users",JSON.stringify(accountList));
+//     form.reset();
+
+//     window.location.href = "login_signup.html";
+// });
+
+// document.querySelector("clear-btn").addEventListener("click",function(){
+//     form.reset();
+// })
+
+
+// here , we will check if the user already have an account, 
+// otherwise , we will add this new user to localStorage
+
+
+
+
+class AccountManager{
+    constructor(){
+        this.storgekey = "myAccounts";
+        const saved = localStorage.getItem(this.storgekey);
+        this.accounts = saved? JSON.parse(saved):[];
+    }
+
+    add(firstname,lastname,username,email,password){
+        const t = new SignUp(firstname,lastname,username,email,password);
+        const exist  = this.accounts.find(a=>a.username === username);
+        if(exist){
+            console.error("Usernaem Already Exist");
+            return;
+        }
+        this.accounts.push(t);
+        this.save();
+        }
+   
+    
+    getAll(){
+        return this.accounts;
+    }
+    save(){
+        localStorage.setItem(this.storgekey,JSON.stringify(this.accounts));
+    }
+
+    login(username,password){
+        const found = this.accounts.find(t=>t.username === username && t.password === password);
+        if(!found){
+            console.error("Invalid username or password");
+            return;
+        }
+        return found;
+    }
+
+
+}
+class SignUp{
+        constructor(firstname,lastname,username,email,password){
+            this.firstname = firstname;
+            this.lastname = lastname;
+            this.username = username;
+            this.email = email;
+            this.password = password;
+            this.createdAt = new Date().toLocaleString();
+        }
+    }
+
+const manager  = new AccountManager();
+
+const submitListener = document.querySelector("#submit-btn");
+submitListener.addEventListener("click",function(e){
     e.preventDefault();
     const firstname = document.querySelector("#first-name").value;
     const lastname = document.querySelector("#last-name").value;
+    const username = document.querySelector("#user-name").value;
     const email = document.querySelector("#email").value;
-    const pass = document.querySelector("#pass").value;
-
-    accountList.push({
-       FName : firstname,
-       LName : lastname,
-       Email : email,
-       Pass : pass
-    });
-
-    localStorage.setItem("users",JSON.stringify(accountList));
-    form.reset();
-
-    window.location.href = "login_signup.html";
+    const password = document.querySelector("#pass").value;
+    manager.add(firstname,lastname,username,email,password);
+    document.querySelector("#personal-info-form").reset();
+    window.location.href = "login.html"
 });
 
-document.querySelector("clear-btn").addEventListener("click",function(){
-    form.reset();
-})
 
-
+const clearListener = document.querySelector("#clear-btn");
+clearListener.addEventListener("click",function(e){
+    document.querySelector("#personal-info-form").reset();
+});
 
